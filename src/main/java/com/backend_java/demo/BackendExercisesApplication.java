d61848f0978afe5db6de4c6759ca3af149df33f7
maxime bru <maximebru@gmail.com>
2024-07-09 00:28:32 +0200

    init spring

---
+++ src/main/java/com/backend_java/demo/BackendExercisesApplication.java
@@ -0,0 +1,13 @@
+package com.backend_java.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendExercisesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend_java/demo/BackendExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend_java.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendExercisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
