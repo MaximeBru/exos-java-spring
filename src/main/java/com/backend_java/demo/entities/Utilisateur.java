150e7f8a02e70c3cf84c752f381ff0e2cee969c2
maxime bru <maximebru@gmail.com>
2024-09-23 19:25:22 +0200

    structure et entity Utilisateur

---
+++ src/main/java/com/backend_java/demo/entities/Utilisateur.java
@@ -0,0 +1,81 @@
+package com.backend_java.demo.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import java.util.Objects;
+
+
+@Entity
+@Table(name = "utilisateurs", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "email")
+})
+public class Utilisateur {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Size(min = 1, max = 30, message = "Le nom d'utilisateur doit comporter entre 1 et 30 caractères")
+    @Column(name = "nom_utilisateur", nullable = false)
+    private String nomUtilisateur;
+
+    @NotBlank(message = "L'adresse email ne peut pas être vide")
+    @Email(message = "L'adresse email doit être valide")
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @NotBlank(message = "Le mot de passe ne peut pas être vide")
+    @Size(min = 6, message = "Le mot de passe doit comporter au moins 6 caractères")
+    @Column(name = "mot_de_passe", nullable = false)
+    private String motDePasse;
+
+    // Getters et Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomUtilisateur() {
+        return nomUtilisateur;
+    }
+
+    public void setNomUtilisateur(String nomUtilisateur) {
+        this.nomUtilisateur = nomUtilisateur;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+
+    public void setMotDePasse(String motDePasse) {
+        this.motDePasse = motDePasse;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Utilisateur that)) return false;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(email, that.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email);
+    }
+}
